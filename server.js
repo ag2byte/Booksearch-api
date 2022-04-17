edb56b9b464da692a47eb233488b911f81634e0c
ag2byte <capsand123@outlook.com>
2022-04-17 12:20:15 +0530

    cors()

--- server.js
+++ server.js
@@ -21,22 +21,22 @@ mongoose.connect(dburi) // connect to the mogodb database
   })
 })
 .catch(err => console.log(err))
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+app.use(cors())
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   )
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "POST, PUT, PATCH, GET, DELETE"
+//     )
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
